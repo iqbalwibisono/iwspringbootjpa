232ed115425c137194918bd167aa41a6f711d046
Iqbal Wibisono <iqbalwibisono28@gmail.com>
2023-02-09 22:20:09 +0700

    Commit Springbootjpa

---
+++ src/main/java/com/juaracoding/iwspringbootjpa/model/Product.java
@@ -0,0 +1,12 @@
+package com.juaracoding.iwspringbootjpa.model;
+/*
+IntelliJ IDEA 2022.3.1 (Community Edition)
+Build #IC-223.8214.52, built on December 20, 2022
+@Author Iqbal Wibisono  a.k.a Iqbal Wibisono
+Java Developer
+Created on 09/02/2023 21:30
+@Last Modified 09/02/2023 21:30
+Version 1.1
+*/
+public class Product {
+}
